da6c3bbdd171032128261113170fb7c0e45f7e4d
EA <eyypaydin@gmail.com>
2017-03-12 19:19:30 +0300

    diamond

---
+++ Diamond.java
@@ -0,0 +1,35 @@
+public class Diamond {
+
+    public static void main(String[] args) {
+
+        System.out.println(diamond(1));
+    }
+
+    public static String diamond(int num) {
+        if (num <= 0 || num%2 == 0)
+            return null;
+
+        StringBuilder lineHolder = new StringBuilder();
+        int numOfLines = 0;
+        int printNum = 1;
+
+        while (numOfLines < num) {
+            String line = "";
+            for (int i = 0; i < Math.abs(num / 2 - (numOfLines)); i++) {
+                line += " ";
+            }
+            for (int i = 0; i < printNum; i++) {
+                line += "*";
+            }
+            if (numOfLines < (num/2))
+                printNum += 2;
+            else
+                printNum -= 2;
+
+            lineHolder.append(line).append("\n");
+            ++numOfLines;
+        }
+
+        return lineHolder.toString();
+    }
+}
